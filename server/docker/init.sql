095c2a5b245574d0b9cfd938b508d13cf2e48495
jaruan <jadon_yuen@outlook.com>
2024-03-08 12:14:48 +0800

    Initialized entity and tables

---
+++ server/docker/init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS `testdb` DEFAULT charset utf8 COLLATE utf8_bin;
+
+USE testdb;
--- server/src/main/java/com/jadon/bookmanagement/entity/Book.java
+++ server/src/main/java/com/jadon/bookmanagement/entity/Book.java
@@ -1,10 +1,43 @@
 package com.jadon.bookmanagement.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "book")
+@AllArgsConstructor
+@Getter
 public class Book {
 
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="title", nullable = false, columnDefinition = "VARCHAR(255)")
+    private String title;
+
+    @Column(name = "author", columnDefinition = "VARCHAR(255)")
+    private String author;
+
+    @Column(name = "publication_year")
+    private LocalDate publicationYear;
+
+    @Column(name = "isbn", columnDefinition = "VARCHAR(17)")
+    private String isbn;
+
+    @Column(name = "created_at", updatable = false, nullable = false)
+    @CreationTimestamp
+    private LocalDate createdAt;
+
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private LocalDate updatedAt;
 
 }
