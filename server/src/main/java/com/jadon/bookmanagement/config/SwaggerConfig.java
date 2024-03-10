ee80e31a7902d1cafb6ed32fda8a6463f9420e88
jaruan <jadon_yuen@outlook.com>
2024-03-10 23:31:23 +0800

    Fixed swagger version issue

--- server/src/main/java/com/jadon/bookmanagement/config/SwaggerConfig.java
+++ server/src/main/java/com/jadon/bookmanagement/config/SwaggerConfig.java
@@ -1,35 +1,35 @@
 package com.jadon.bookmanagement.config;
 
 
-import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootConfiguration
-@EnableOpenApi
-public class Swagger3Config {
-
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.OAS_30)
-                .apiInfo(apiInfo())
-                .enable(false)
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket buildDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(buildApiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .apis(RequestHandlerSelectors.basePackage("com.jadon.bookmanagement"))
                 .paths(PathSelectors.any())
                 .build();
+
     }
 
-    private ApiInfo apiInfo() {
+    private ApiInfo buildApiInfo() {
         return new ApiInfoBuilder()
-                .title("Swagger3 API docs")
-                .description("Book Management System API docs")
-                .version("1.0")
-                .build();
+                .title("Book-Management-swagger")
+                .description("Book Management API docs")
+                .version("1.0").build();
     }
+
 }
